ac747f09f965091c9415f550b770091bc92d5f7d
ff311_school <1328469615@qq.com>
2025-11-05 11:06:16 +0800

    用户操作拦截过滤器，并将用户DTO放到线程存储中

---
+++ admin/src/main/java/com/fengjie/shortlink/admin/common/biz/user/UserTransmitFilter.java
@@ -0,0 +1,28 @@
+package com.fengjie.shortlink.admin.common.biz.user;
+
+import cn.hutool.core.util.StrUtil;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class UserTransmitFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        String username = httpServletRequest.getHeader("username");
+        if (StrUtil.isNotBlank(username)) {
+            String userId = httpServletRequest.getHeader("userId");
+            String realName = httpServletRequest.getHeader("realName");
+            UserInfoDTO userInfoDTO = new UserInfoDTO(userId, username, realName);
+            UserContext.setUser(userInfoDTO);
+        }
+        try {
+            filterChain.doFilter(servletRequest, servletResponse);
+        } finally {
+            UserContext.removeUser();
+        }
+    }
+}
